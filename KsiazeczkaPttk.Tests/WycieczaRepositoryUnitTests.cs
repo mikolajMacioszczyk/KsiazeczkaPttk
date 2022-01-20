88ff5835037a4df5f989566fd7184ff906c48510
Mikołaj <macioszczyk.mikolaj@gmail.com>
2022-01-20 21:29:29 +0100

    test wycieczka creation

--- KsiazeczkaPttk.Tests/WycieczaRepositoryUnitTests.cs
+++ KsiazeczkaPttk.Tests/WycieczaRepositoryUnitTests.cs
@@ -28,7 +28,8 @@ namespace KsiazeczkaPttk.Tests
             await KsiazeczkaSeed.Seed(_context);
             var wycieczka = new Wycieczka()
             {
-                Wlasciciel = string.Empty
+                Wlasciciel = string.Empty,
+                Odcinki = new List<PrzebycieOdcinka>() { new PrzebycieOdcinka()}
             };
 
             // act
@@ -50,7 +51,7 @@ namespace KsiazeczkaPttk.Tests
                 Wlasciciel = "Turysta1",
                 Odcinki = new List<PrzebycieOdcinka>() 
                 {
-                    new PrzebycieOdcinka() { OdcinekId = 1000 }
+                    new PrzebycieOdcinka() { OdcinekId = 1000, Kolejnosc = 1 }
                 }
             };
 
@@ -65,16 +66,12 @@ namespace KsiazeczkaPttk.Tests
 
 
         [Fact]
-        public async Task CreateWycieczka_Poprawna()
+        public async Task CreateWycieczka_Empty()
         {
             // arrange
             await KsiazeczkaSeed.Seed(_context);
-            string nazwa = "Poprawna Wycieczka";
-            var odcinki = new List<PrzebycieOdcinka>()
-                {
-                    new PrzebycieOdcinka() { OdcinekId = 1, Kolejnosc = 1 },
-                    new PrzebycieOdcinka() { OdcinekId = 2, Kolejnosc = 2, Powrot = true }
-                };
+            string nazwa = Guid.NewGuid().ToString();
+            var odcinki = new List<PrzebycieOdcinka>();
 
             var wycieczka = new Wycieczka()
             {
@@ -87,14 +84,111 @@ namespace KsiazeczkaPttk.Tests
             var result = await _wycieczkaRepository.CreateWycieczka(wycieczka);
 
             // assert
-            Assert.True(result.IsSuccesful);
-            Assert.NotNull(result.Value);
-            Assert.Equal(nazwa, result.Value.Nazwa);
-            Assert.Equal(Domain.Enums.StatusWycieczki.Planowana, result.Value.Status);
-            Assert.True(odcinki.Select(o => (o.OdcinekId, o.Kolejnosc)).SequenceEqual(result.Value.Odcinki.Select(o => (o.OdcinekId, o.Kolejnosc))));
-            Assert.NotNull(await _context.Wycieczki.FirstOrDefaultAsync(w => w.Id == result.Value.Id));
+            Assert.False(result.IsSuccesful);
+            Assert.Null(result.Value);
+            Assert.Equal("Pusta wycieczka", result.Message);
         }
 
-        // Testy kolejności
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(2, false)]
+        [InlineData(0, false)]
+        public async Task CreateWycieczka_JedenOdcinek(int kolejnosc, bool isOk)
+        {
+            // arrange
+            await KsiazeczkaSeed.Seed(_context);
+            string nazwa = Guid.NewGuid().ToString();
+            var odcinki = new List<PrzebycieOdcinka>()
+            {
+                new PrzebycieOdcinka(){Kolejnosc = kolejnosc, OdcinekId = 1 }
+            };
+
+            var wycieczka = new Wycieczka()
+            {
+                Wlasciciel = "Turysta1",
+                Nazwa = nazwa,
+                Odcinki = new List<PrzebycieOdcinka>(odcinki)
+            };
+
+            // act
+            var result = await _wycieczkaRepository.CreateWycieczka(wycieczka);
+
+            // assert
+            if (isOk)
+            {
+                Assert.True(result.IsSuccesful);
+                Assert.NotNull(result.Value);
+                Assert.Equal(nazwa, result.Value.Nazwa);
+                Assert.Equal(Domain.Enums.StatusWycieczki.Planowana, result.Value.Status);
+                Assert.True(odcinki.Select(o => (o.OdcinekId, o.Kolejnosc)).SequenceEqual(result.Value.Odcinki.Select(o => (o.OdcinekId, o.Kolejnosc))));
+                Assert.NotNull(await _context.Wycieczki.FirstOrDefaultAsync(w => w.Id == result.Value.Id));
+            }
+            else
+            {
+                Assert.False(result.IsSuccesful);
+                Assert.Null(result.Value);
+                Assert.Equal("Niepoprawna kolejność odcinków", result.Message);
+            }
+        }
+
+        [Theory]
+        // dwa odcinki o tej samej kolejnosci
+        [InlineData(1, 1, false, 1, 2, false, false, "Niepoprawna kolejność odcinków")]
+        // dwa odcinki o niepasującej kolejnosci
+        [InlineData(1, 1, false, 4, 2, false, false, "Niepoprawna kolejność odcinków")]
+        // dwa odcinki o pasującej kolejności bez powrotów
+        [InlineData(1, 3, false, 2, 2, false, true, "")]
+        // dwa odcinki o nie połączonych punktach bez powrotow
+        [InlineData(1, 1, false, 2, 4, false, false, "Odcinki o kolejności: 1 oraz 2 nie są połączone")]
+        // dwa odcinki, w tym jeden powrót
+        [InlineData(1, 1, false, 2, 2, true, true, "")]
+        // dwa odcinki o nie połączonych punktach, w tym jeden powrót
+        [InlineData(1, 1, false, 2, 3, true, false, "Odcinki o kolejności: 1 oraz 2 nie są połączone")]
+        // dwa odcinki, obydwa powroty
+        [InlineData(1, 3, true, 2, 4, true, true, "")]
+        // dwa odcinki o nie połączonych punktach, obydwa powroty
+        [InlineData(1, 2, true, 2, 4, true, false, "Odcinki o kolejności: 1 oraz 2 nie są połączone")]
+        // ten sam odcinek wraz z powrotem
+        [InlineData(1, 1, false, 2, 1, true, true, "")]
+        // ten sam odcinek wraz z powrotem w złej kolejności
+        [InlineData(1, 1, true, 2, 1, false, true, "Odcinki o kolejności: 1 oraz 2 nie są połączone")]
+        public async Task CreateWycieczka_DwaOdcinkiOdcinek(int kolejnoscPierwszego, int idPierwszego, bool powrotPierwszego, int kolejnoscDrugiego, int idDrugiego, bool powrotDrugiego, bool isOk, string reason)
+        {
+            // arrange
+            await KsiazeczkaSeed.Seed(_context);
+            string nazwa = Guid.NewGuid().ToString();
+            var odcinki = new List<PrzebycieOdcinka>()
+            {
+                new PrzebycieOdcinka(){Kolejnosc = kolejnoscPierwszego, OdcinekId = idPierwszego, Powrot = powrotPierwszego },
+                new PrzebycieOdcinka(){Kolejnosc = kolejnoscDrugiego, OdcinekId = idDrugiego, Powrot = powrotDrugiego },
+            };
+
+            var wycieczka = new Wycieczka()
+            {
+                Wlasciciel = "Turysta1",
+                Nazwa = nazwa,
+                Odcinki = new List<PrzebycieOdcinka>(odcinki)
+            };
+
+            // act
+            var result = await _wycieczkaRepository.CreateWycieczka(wycieczka);
+
+            // assert
+            if (isOk)
+            {
+                Assert.True(result.IsSuccesful);
+                Assert.NotNull(result.Value);
+                Assert.Equal(nazwa, result.Value.Nazwa);
+                Assert.Equal(Domain.Enums.StatusWycieczki.Planowana, result.Value.Status);
+                Assert.True(odcinki.Select(o => (o.OdcinekId, o.Kolejnosc)).SequenceEqual(result.Value.Odcinki.Select(o => (o.OdcinekId, o.Kolejnosc))));
+                Assert.NotNull(await _context.Wycieczki.FirstOrDefaultAsync(w => w.Id == result.Value.Id));
+            }
+            else
+            {
+                Assert.False(result.IsSuccesful);
+                Assert.Null(result.Value);
+                Assert.Equal(reason, result.Message);
+            }
+        }
     }
 }
